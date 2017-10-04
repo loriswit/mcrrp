85840d40df25ba4f0b030b4d5ebe5ceca79591cc
Loris Witschard <loris.wit@gmail.com>
2017-10-04 11:47:09 +0200

    Add login with Mojang credentials

--- app/page/login.php
+++ app/page/login.php
@@ -14,34 +14,41 @@ class Login extends Page
     
     protected function run()
     {
+        $this->set("online", CONFIG["settings"]["online"]);
     }
     
     protected function submit()
     {
         $name = $_POST["username"];
-        $online = CONFIG["settings"]["online"];
         
-        if($online)
-            $json = @file_get_contents("https://api.mojang.com/users/profiles/minecraft/$name");
-        else
-            $json = @file_get_contents("https://www.fabianwennink.nl/projects/OfflineUUID/api/$name");
-        
-        $data = json_decode($json, true);
-        
-        if($data == false || isset($data["error"]))
-            throw new InvalidInputException("Invalid username. Please try again.");
-        
-        if($online)
+        if(CONFIG["settings"]["online"])
         {
-            $uuid = $data["id"];
+            $MCAuth = new MCAuth\Api();
+            
+            try
+            {
+                $account = $MCAuth->sendAuth($name, $_POST["password"]);
+            }
+            catch(Exception $e)
+            {
+                throw new InvalidInputException("Invalid username or password.");
+            }
+            
+            $uuid = $account->uuid;
             $uuid = substr_replace($uuid, "-", 8, 0);
             $uuid = substr_replace($uuid, "-", 13, 0);
             $uuid = substr_replace($uuid, "-", 18, 0);
             $uuid = substr_replace($uuid, "-", 23, 0);
-            $username = $data["name"];
+            $username = $account->username;
         }
         else
         {
+            $json = @file_get_contents("https://www.fabianwennink.nl/projects/OfflineUUID/api/$name");
+            $data = json_decode($json, true);
+            
+            if($data == false || isset($data["error"]))
+                throw new InvalidInputException("Invalid username.");
+            
             $uuid = $data["uuid"];
             $username = $data["username"];
         }
