6c0be591d3af9680c40ab1a1e32362b0866f8f30
Loris Witschard <loris.wit@gmail.com>
2017-10-20 23:25:16 +0200

    Reformat items and avatars icons

--- app/page/api.php
+++ app/page/api.php
@@ -1,44 +1,52 @@
 <?php
 
 /**
- * Web API providing informations about valid codes.
+ * Web API providing various resources.
  */
 class Api extends Page
 {
     protected $argsCount = 2;
     
-    protected function title()
-    {
-        return "api";
-    }
-    
     protected function run()
     {
-        if(count($this->args) == 2)
+        if(count($this->args) != 2)
         {
-            $value = strtoupper($this->args[1]);
-            switch($this->args[0])
-            {
-                case "icon":
-                    if(strpos($value, "-") === false)
-                        $value .= "-0";
-                    
-                    header("Location: https://olybri.github.io/bing/icons/$value.png");
-                    exit;
-                
-                case "code":
-                    if(!LOGGED || $value == $this->citizen["code"])
-                        echo 0;
-                    else
-                        echo $this->db->citizenExists($this->args[1]) ? 1 : 0;
-                    exit;
-            }
+            header("Location: /");
+            exit;
         }
-        else
-            Header("Location: /");
+        
+        $value = strtoupper($this->args[1]);
+        switch($this->args[0])
+        {
+            case "icon":
+                if(strpos($value, "-") === false)
+                    $value .= "-0";
+                
+                header("Location: https://olybri.github.io/bing/icons/$value.png");
+                exit;
+            
+            case "avatar":
+                header("Location: https://crafatar.com/avatars/$value?size=32&overlay");
+                exit;
+            
+            case "code":
+                if(!LOGGED || $value == $this->citizen["code"])
+                    echo 0;
+                else
+                    echo $this->db->citizenExists($this->args[1]) ? 1 : 0;
+                exit;
+            
+            default:
+                header("Location: /");
+                exit;
+        }
     }
     
     protected function submit()
+    {
+    }
+    
+    protected function title()
     {
     }
 }
